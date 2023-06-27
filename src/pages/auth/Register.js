0156823ad99c92a817da94a1f97f1af4d9bd8216
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-06-27 10:40:02 +0100

    setting up firebase

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./auth.module.scss";
 import registerImg from "../../assests/register.png";
 import { Link } from "react-router-dom";
 import Card from "../../components/card/Card";
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+
+  const registerUser = (e) =>{
+    e.preventDefault()
+    console.log(email,password,cPassword)
+  }
   return (
     <section className={`container ${styles.auth}`}>
       <Card>
         <div className={styles.form}>
           <h2>Register</h2>
 
-          <form>
-            <input type="text" placeholder="Email" required />
-            <input type="password" placeholder="Password" required />
-            <input type="password" placeholder=" Confirm Password" required />
-            <button className="--btn --btn-primary --btn-block">Login</button>
+          <form onSubmit={registerUser}>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              placeholder="Email"
+              required
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              placeholder="Password"
+              required
+            />
+            <input
+              type="password"
+              value={cPassword}
+              onChange={(e) => {
+                setCPassword(e.target.value);
+              }}
+              placeholder=" Confirm Password"
+              required
+            />
+            <button type="submit" className="--btn --btn-primary --btn-block">Register</button>
           </form>
 
           <span className={styles.register}>
